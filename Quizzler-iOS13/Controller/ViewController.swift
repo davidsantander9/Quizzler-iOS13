759c6678c9e799551d2af840affded541a78a321
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-11-01 17:33:27 -0600

    implement mvc

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -21,30 +21,16 @@ class ViewController: UIViewController {
     // Variables
     var countdownTimerToAnswer: Timer?
     var countdownTimerFeedBackAnswer: Timer?
-    var questionNumber = 0
     var timePassedToAnswer = 0
     var secondsRemainingToAnswer = 0
     var timePassedToShowFeedback = 0
     var secondsRemainingFeedback = 0
     
+    var quizLogic = QuizLogic()
+    
     // Constants
     let secondsToAnswer = 30
     let secondsToShowFeedBack = 3
-    let quizQuestions = [
-        Question(q: "A slug's blood is green.", a: true),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: true),
-        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: true),
-        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: true),
-        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: false),
-        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: true),
-        Question(q: "You can lead a cow down stairs but not up stairs.", a: false),
-        Question(q: "Google was originally called 'Backrub'.", a: true),
-        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: true),
-        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: false),
-        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: false),
-        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.",a: true)
-    ]
-    
     
     
     override func viewDidLoad() {
@@ -58,7 +44,7 @@ class ViewController: UIViewController {
     
     @IBAction func pressedAcctionButton(_ sender: UIButton) {
         let userAnswer = sender.currentTitle?.boolValue
-        let correctAnswer = quizQuestions[questionNumber].answer
+        let isCorrect = quizLogic.checkAnswer(answer: userAnswer!)
         
         timePassedToAnswer = 0
         secondsRemainingToAnswer = secondsToAnswer
@@ -69,23 +55,16 @@ class ViewController: UIViewController {
         startTimerToAnswer()
         startTimerToShowFeedBack()
         
-        if userAnswer == correctAnswer {
-            sender.backgroundColor = UIColor.green
-        } else{
-            sender.backgroundColor = UIColor.red
-        }
+        sender.backgroundColor = isCorrect ? UIColor.green : UIColor.red
         
-        if questionNumber < quizQuestions.count - 1   {
-            questionNumber += 1
-            updateQuestionUI()
-        } else {
-            questionNumber = 0
-        }
+        quizLogic.moveToNextQuestion()
+        
+        updateQuestionUI()
     }
     
     func updateQuestionUI(){
-        questionLabel.text = quizQuestions[questionNumber].text
-        progressBarQuestion.progress = Float(questionNumber + 1) / Float(quizQuestions.count)
+        questionLabel.text = quizLogic.getQuestionText()
+        progressBarQuestion.progress = quizLogic.getQuizProgress()
     }
     
     func startTimerToAnswer(){
@@ -98,7 +77,7 @@ class ViewController: UIViewController {
     
     @objc func updateTimer2() {
            // Increment the time passed
-           timePassedToShowFeedback += 1 
+           timePassedToShowFeedback += 1
         if timePassedToShowFeedback == secondsRemainingFeedback {
             trueButton.backgroundColor = UIColor.clear
             falseButton.backgroundColor = UIColor.clear
---
+++ Quizzler-iOS13/Model/QuizLogic.swift
@@ -0,0 +1,49 @@
+//
+//  QuizLogic.swift
+//  Quizzler-iOS13
+//
+//  Created by David C Santander on 01/11/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct QuizLogic{
+    
+    let quizQuestions = [
+        Question(q: "A slug's blood is green.", a: true),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: true),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: true),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: true),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: false),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: true),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: false),
+        Question(q: "Google was originally called 'Backrub'.", a: true),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: true),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: false),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: false),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.",a: true)
+    ]
+    
+    var questionNumber = 0
+    
+    func checkAnswer(answer userAnswer: Bool) -> Bool{
+        return userAnswer == quizQuestions[questionNumber].answer
+    }
+    
+    func getQuizProgress() -> Float {
+        return Float(questionNumber + 1) / Float(quizQuestions.count)
+    }
+    
+    func getQuestionText() -> String{
+        return quizQuestions[questionNumber].text
+    }
+    
+    func isQuizComplete() -> Bool {
+        return questionNumber < quizQuestions.count - 1
+    }
+    
+    mutating func moveToNextQuestion(){
+        questionNumber = isQuizComplete() ? questionNumber + 1 : 0
+    }
+}
